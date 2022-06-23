b306d8227f2c52f6a574fb73a541b5cee99a36f8
Daniel <saruman566@yahoo.de>
2022-06-23 18:11:30 +0200

    first version

---
+++ main.py
@@ -0,0 +1,38 @@
+
+abc = {
+    'a': '.-',
+    'b': '-...',
+    'c': '-.-.',
+    'd': '-..',
+    'e': '.',
+    'f': '..-.',
+    'g': '--.',
+    'h': '....',
+    'i': '..',
+    'j': '.---',
+    'k': ' -.-',
+    'l': '.-..',
+    'm': '--',
+    'n': '-.',
+    'o': '---',
+    'p': '.--.',
+    'q': '--.-',
+    'r': '.-.',
+    's': '...',
+    't': '-',
+    'u': '..-',
+    'v': '...-',
+    'w': '.--',
+    'x': '-..-',
+    'y': '-.--',
+    'z': ' --..'
+}
+
+words = list(input('Please choose your Word.').lower())
+
+morscode = []
+
+for letter in words:
+    morscode.append(abc.get(letter))
+
+print(f'Your Word in Morse Code is {morscode}')
